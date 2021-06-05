9d2d374713d599f584f01def2791a8542776d761
bgolden <>
2021-06-05 18:17:07 +0000

    submitting code

--- candidate-testing.js
+++ candidate-testing.js
@@ -14,8 +14,8 @@ let candidateAnswers = "";
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-	let name = input.question("Candidate name: ");
-	return name;
+	let candidateName = input.question("Candidate name: ");
+	return candidateName;
 }
 
 
